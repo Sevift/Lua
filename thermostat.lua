7429c4c8d79f488e977563c0bb1a7f91f481e6eb
Sevift <sevift@gmail.com>
2017-03-07 16:55:42 +0100

    Added notes

--- thermostat.lua
+++ thermostat.lua
@@ -3,7 +3,7 @@ commandArray = {}
 if devicechanged['TempHum Hall_Temperature'] == nil and
     devicechanged['TempHum Bedroom_Temperature'] == nil and
     devicechanged['TempHum Children_Temperature'] == nil then
-    return commandArray
+    return commandArray -- если это не внешние датчики температуры, то ни чего не делаем 
 end
 --//////////////////////////////////////////////////////////////////////////////
 
@@ -12,23 +12,23 @@ function Round(num, idp) -- округление, которгго нет них
   return math.floor(num * mult + 0.5) / mult
 end
 
-local minTemp = 18
-local maxTemp = 35
-local delta = 0.2
+local minTemp = 18 -- максимальная температура термостата
+local maxTemp = 35 -- минимальная температура термостата
+local delta = 0.2 -- разница температур вниз при которой ни чего не делаем
  
 rooms = {'Hall', 'Hall Tele', 'Bedroom', 'Children'}
 
 for room in pairs(rooms) do 
 
-	local roomName       = rooms[room]
+	local roomName       = rooms[room] --- имя комнаты
 	local comfortName    = 'Comfort setpoint '..roomName -- имя термостата
 	roomName = string.gsub(roomName, ' Tele', '')
 
-	local comfort  = Round(tonumber(otherdevices_svalues[comfortName]), 1)  -- текущее значение Comfort
+	local comfort  = Round(tonumber(otherdevices_svalues[comfortName]), 1)  -- текущее значение Comfort (температуры режима Comfort)
 	local tempNeed = Round(tonumber(otherdevices_svalues['Thermostat '..roomName]), 1) -- текущее значение температуры вирт термостата
-	local tempHum  = Round(tonumber(otherdevices_temperature['TempHum '..roomName]), 1) -- текущее значение tempHum
+	local tempHum  = Round(tonumber(otherdevices_temperature['TempHum '..roomName]), 1) -- текущее значение tempHum (внешний датчик температуры)
 	
-	local difference = tempNeed - tempHum
+	local difference = tempNeed - tempHum -- разница между нужной температуры и двнешнего атчика температуры
 	if difference > 0 and difference <= delta then 
 	    difference = 0   
 	end    

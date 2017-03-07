439870b03548751acd608eed7f1a5d007c675565
Sevift <sevift@gmail.com>
2017-03-07 19:26:43 +0100

    Added notes

--- thermostat.lua
+++ thermostat.lua
@@ -1,3 +1,6 @@
+-- Добавляем виртуальные термостаты - в них будем устанавливать нужную температуру в комнатах
+-- В звисимостти от установленной температуры и внешних датчиков температуры будем управлять реальными терсомтатами
+
 commandArray = {}
 
 if devicechanged['TempHum Hall_Temperature'] == nil and
@@ -16,12 +19,12 @@ local minTemp = 18 -- максимальная температура термо
 local maxTemp = 35 -- минимальная температура термостата
 local delta = 0.2 -- разница температур вниз при которой ни чего не делаем
  
-rooms = {'Hall', 'Hall Tele', 'Bedroom', 'Children'}
+rooms = {'Hall', 'Hall Tele', 'Bedroom', 'Children'} -- комнаты
 
 for room in pairs(rooms) do 
 
 	local roomName       = rooms[room] --- имя комнаты
-	local comfortName    = 'Comfort setpoint '..roomName -- имя термостата
+	local comfortName    = 'Comfort setpoint '..roomName -- имя режима Comfort реального термостата
 	roomName = string.gsub(roomName, ' Tele', '') -- Вырезаем из имени термостата 'Tele' чтобы скрипт думал, что это один термостат так как термостаты Hall и Hall Tele находятся в одной комнате, а значит управляем ими одинаково
 
 	local comfort  = Round(tonumber(otherdevices_svalues[comfortName]), 1)  -- текущее значение Comfort (температуры режима Comfort)
